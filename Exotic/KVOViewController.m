bf1acfcc57004b2d87c5d33281c548ecae5ab7aa
chenww <862783002@qq.com>
2016-10-21 11:56:31 +0800

    KVO Bug

--- Exotic/KVOViewController.m
+++ Exotic/KVOViewController.m
@@ -38,7 +38,7 @@
     if([keyPath isEqualToString:@"num"])
     {
         // 响应变化处理：UI更新（label文本改变）
-        self.numLab.text = [NSString stringWithFormat:@"当前的num值为：%@",[change valueForKey:@"new"]];
+        self.numLab.text =  [NSString stringWithFormat:@"当前的num值为：%@",[change valueForKey:@"new"]];
         
         //上文注册时，枚举为2个，因此可以提取change字典中的新、旧值的这两个方法
         NSLog(@"\noldnum:%@ newnum:%@",[change valueForKey:@"old"],[change valueForKey:@"new"]);
